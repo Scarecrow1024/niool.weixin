f1d45667dc61e62b5fce6d01f88c15ea2386f271
Scarecrow1024 <819681825@qq.com>
2016-09-21 16:29:22 +0800

    分享测试

--- Addons/BxqScore/Controller/BxqScoreController.class.php
+++ Addons/BxqScore/Controller/BxqScoreController.class.php
@@ -326,14 +326,24 @@ class BxqScoreController extends AddonsController{
     public function share(){
         $appid = 'wxc5d2229adc0102a3';
         $secret = '886d44b2cfec214e8d752b731e5da432 ';
-        echo access_token();
-        die;
+        /*$auth = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$secret."&grant_type=authorization_code");//通过code换取网页授权access_token
+        $jsonauth = json_decode($auth); //对JSON格式的字符串进行编码
+        $arrayauth = get_object_vars($jsonauth);//转换成数组
+        $openid = $arrayauth['openid'];//输出openid
+        $access_token = $arrayauth['access_token'];
+        $_SESSION['openid'] = $openid;
+        
+        $accesstoken = file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$secret."");//获取access_token
+        $token = json_decode($accesstoken); //对JSON格式的字符串进行编码
+        $t = get_object_vars($token);//转换成数组*/
+        $access_token = 'fsmU40Bgan35nKkHo0NjBZPoaQ6ywUA1RXHrWxO2iP34cQoHbc8yPb_4yhsATmiOOM-_aHyAPtrXSPBtEHwW-qfL8yNGg6p95AarVlpemp_tXp3eNege6k8zLGlkloZhMZGhAJAZMT';
         
         $jsapi = file_get_contents("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$access_token."&type=jsapi");
         $jsapi = json_decode($jsapi);
         $j = get_object_vars($jsapi);
         $jsapi = $j['ticket'];//get JSAPI
-        
+        echo $jsapi;
+        die;
         $time = 14999923234;
         $noncestr= $time;
         $jsapi_ticket= $jsapi;
