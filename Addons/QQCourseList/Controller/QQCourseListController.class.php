4a239298f17ea0548094d0bf6d34881eb496196f
Scarecrow1024 <819681825@qq.com>
2016-09-26 16:02:02 +0800

    qq

--- Addons/QQCourseList/Controller/QQCourseListController.class.php
+++ Addons/QQCourseList/Controller/QQCourseListController.class.php
@@ -9,7 +9,7 @@ class QQCourseListController extends AddonsController{
         $openid=$_SESSION['openid'];
         $user=M('qqcourselist');
         //$data=$user->where("openid=".'"'.$openid.'"')->getField('courselist');
-        $data = $user->limit(1)->getField('courselist');
+        $data = $user->getField('courselist');
         $day=json_decode($data,true); 
         print($day);
         die;   
