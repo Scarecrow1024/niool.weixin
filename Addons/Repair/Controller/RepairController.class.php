1c4c3ffa2a686caf33202db4cbf1af969c4980eb
niool <819681825@qq.com>
2016-10-22 18:10:54 +0800

    nqbx

--- Addons/Repair/Controller/RepairController.class.php
+++ Addons/Repair/Controller/RepairController.class.php
@@ -63,7 +63,7 @@ class RepairController extends AddonsController{
         $user=M('user');
         $studentid = $user->where("openid=".'"'.$openid.'"')->getField('studentid');
         $IdCard = $user->where("openid=".'"'.$openid.'"')->getField('IdCard');
-        $mm = substr($IdCard, 11, 6);
+        $mm = substr($IdCard, 12, 6);
         $log_url="http://218.196.240.133/nqwx/index.jsp";
         $log_post="username=".$studentid."&password=".$mm."&Submit2=%B5%C7%C2%BD&ihide=yhide";
         $ch=curl_init();
