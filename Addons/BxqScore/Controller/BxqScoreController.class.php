11fef0639f3e6bb4dfe32c8883619bf86ba703af
zyf <819681825@qq.com>
2017-01-15 14:39:48 +0800

    增加退出功能

--- Addons/BxqScore/Controller/BxqScoreController.class.php
+++ Addons/BxqScore/Controller/BxqScoreController.class.php
@@ -307,7 +307,7 @@ class BxqScoreController extends AddonsController{
         }
         $con=count($data['kcm']);
         if($con==0){
-            echo "vpn已达到最大限制人数,请返回刷新验证码并重新登录";
+            echo "<h2>教务处VPN已达到最大限制人数,请返回刷新验证码并重新登录</h2>";
             die;
         }
         //print_r($arr)."<br>";
