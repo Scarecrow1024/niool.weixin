5a5eebca1d8d034858a3de0302e2a367c7d46edc
zyf <819681825@qq.com>
2017-01-15 13:53:42 +0800

    增加退出功能

--- Addons/BxqScore/Controller/BxqScoreController.class.php
+++ Addons/BxqScore/Controller/BxqScoreController.class.php
@@ -135,24 +135,6 @@ class BxqScoreController extends AddonsController{
         curl_close($ch);
         echo $content;
 
-        //退出
-        $ch=curl_init();
-        $url="https://vpn.hpu.edu.cn/por/logout.csp?rnd=9161307384583139";
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_HEADER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查 
-        curl_setopt($ch,CURLOPT_REFERER,"https://vpn.hpu.edu.cn/por/login_psw.csp");
-        curl_setopt($ch,CURLOPT_USERAGENT , "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0");
-        curl_setopt($ch,CURLOPT_COOKIE,"$cookie2;$cookie3;$cookie4"); 
-        setcookie("isl","0");
-        setcookie("TWFID","deleted");
-        setcookie("expires","Saturday, 16-Jan-16 13:41:29 GMT");
-        setcookie("path","/");
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        $logout=curl_exec($ch);
-        curl_close($ch);
-        echo $logout;
-
         /*$xmlstr=$content;
         $openid=get_openid();
         if($openid=='-1'){
@@ -321,6 +303,24 @@ class BxqScoreController extends AddonsController{
         //print_r($arr)."<br>";
         $this->assign('data',$data);
         $this->assign('con',$con);
+
+        //退出
+        $ch=curl_init();
+        $url="https://vpn.hpu.edu.cn/por/logout.csp?rnd=9161307384583139";
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查 
+        curl_setopt($ch,CURLOPT_REFERER,"https://vpn.hpu.edu.cn/por/login_psw.csp");
+        curl_setopt($ch,CURLOPT_USERAGENT , "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0");
+        curl_setopt($ch,CURLOPT_COOKIE,"$cookie2;$cookie3;$cookie4"); 
+        setcookie("isl","0");
+        setcookie("TWFID","deleted");
+        setcookie("expires","Saturday, 16-Jan-16 13:41:29 GMT");
+        setcookie("path","/");
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        $logout=curl_exec($ch);
+        curl_close($ch);
+        
         setcookie("isl",null);
         setcookie($cookie2,null);
         setcookie($cookie3,null);
