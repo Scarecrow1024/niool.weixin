87d4d1f2650a1f76e4cf7e50910227402c5fdbc2
zyf <819681825@qq.com>
2017-01-16 10:45:53 +0800

    test

--- Addons/Binding/Controller/BindingController.class.php
+++ Addons/Binding/Controller/BindingController.class.php
@@ -207,6 +207,24 @@ class BindingController extends AddonsController{
         curl_close($ch);
         echo $content;
 
+        //退出
+        $ch=curl_init();
+        $url="https://vpn.hpu.edu.cn/por/logout.csp?rnd=9161307384583139";
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查 
+        curl_setopt($ch,CURLOPT_REFERER,"https://vpn.hpu.edu.cn/por/login_psw.csp");
+        curl_setopt($ch,CURLOPT_USERAGENT , "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0");
+        curl_setopt($ch,CURLOPT_COOKIE,"$session2;$session3;$session4"); 
+        setcookie("isl","0");
+        setcookie("TWFID","deleted");
+        setcookie("expires","Saturday, 16-Jan-16 13:41:29 GMT");
+        setcookie("path","/");
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        $logout=curl_exec($ch);
+        curl_close($ch);
+        session_destroy()
+
         /*$data = $this->get_info();
         echo $data[0]['studentid']."<br>";
         echo substr($data[0]['IdCard'],12);*/
