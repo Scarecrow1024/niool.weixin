6a5124aa222e3c9ed8875eaa38ffd194dea938b0
zyf <819681825@qq.com>
2017-04-23 10:51:14 +0800

    md jwc

--- Addons/Lib/Controller/LibController.class.php
+++ Addons/Lib/Controller/LibController.class.php
@@ -174,9 +174,9 @@ class LibController extends AddonsController{
         return $info;*/
         return $info;
     }
-    
+    date_default_timezone_set("Asia/Shanghai");
     public function grzx(){
-        $time=date('G')*3600+date('i')*60+date('s')-300;
+        $time=date('G')*3600+date('i')*60+date('s')-290;
         $tday1=$time;
         $user=M('user');
         $openid=get_openid();
