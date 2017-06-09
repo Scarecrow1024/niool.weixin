7163863335eddf8e641a55e85e417e8fce96c1c1
zyf <819681825@qq.com>
2017-06-09 22:25:27 +0800

    qq callback

--- QQConnect/callback.php
+++ QQConnect/callback.php
@@ -25,10 +25,10 @@
 	$result=mysql_query($sql,$link);
 	$ret=mysql_fetch_assoc($result);
 	if($ret!=false){
-		$url="http://blog.niool.com/weixin/index.php?s=/addon/QQCourseList/QQCourseList/course.html";
+		$url="https://blog.niool.com/weixin/index.php?s=/addon/QQCourseList/QQCourseList/course.html";
 		header("location:$url");
 	}else{
-		$url="http://blog.niool.com/weixin/index.php?s=/addon/QQBind/QQBind/login.html";
+		$url="https://blog.niool.com/weixin/index.php?s=/addon/QQBind/QQBind/login.html";
 		header("location:$url");
 	}
 
